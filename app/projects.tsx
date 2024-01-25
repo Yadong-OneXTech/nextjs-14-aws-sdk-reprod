4515ea2d2089281c850230ef8de0ea376896e907
Yadong Zhang <yadong@onextech.com>
2024-01-25 17:25:00 +0800

    add @supabase/supabase-js deps.

--- app/projects.tsx
+++ app/projects.tsx
@@ -1,7 +1,13 @@
-import { fetchDynamoDbProjectBySlug } from "./actions"
+import { fetchDynamoDbProjectBySlug, fetchDynamoDb, fetchProjectBySlug, fetchProjectRentalTransactions, fetchProjectSalesTransactions, fetchS3SignedUrl, getUser } from "./actions"
 
 export default async function Page(props: any) {
     const { data, error } = await fetchDynamoDbProjectBySlug('foo')
+    const x = await fetchDynamoDb('ScanFilter' as any)
+    const y = await fetchProjectBySlug('foo')
+    const z = await fetchProjectRentalTransactions({ projectTitle: 'foo' })
+    const r = await fetchProjectSalesTransactions({ projectTitle: 'foo', salesInterval: 'month' })
+    const s = await fetchS3SignedUrl({ Bucket: 'public' })
+    const t = await getUser()
 
     return <div>Project</div>
 }
